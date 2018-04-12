5da3b6e430f58e77d14a8560c2f0d97c27955d58
Kayla Reopelle <kreopelle@gmail.com>
2018-04-12 13:04:05 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -14,16 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        :friend_1 => {
-          :name => "Steven",
-          :age => "30",
-          :attitude => "confused"
-        },
-        :friend_2 => {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
-        }
+        :friend_1 => {:name => "Steven", :age => "30", :attitude => "confused"},
+        :friend_2 => {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
